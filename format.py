3321f6f9d9e5f5a089ac0cf520b2260dd618451a
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-10 01:23:59 +0100

    Collect date as user input

---
+++ format.py
@@ -0,0 +1,17 @@
+def main():
+    months = [
+        'January','February','March','April','May','June',
+        'July','August','September','October','November','December'
+    ]
+    while True:     
+        #Collect date as user input 
+        date = input('Date: ').strip()
+
+        #Check format of input date string and split
+        
+
+
+
+
+if __name__ == "__main__":
+    main()
